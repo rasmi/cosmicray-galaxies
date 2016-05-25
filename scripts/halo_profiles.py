ccdc9d632c17518590c577acba752a3210ccce8d
Rasmi Elasmar <rrelasmar@gmail.com>
2016-05-25 16:33:28 -0400

    Added profile plots (need to fix duplicate issue).

--- scripts/halo_profiles.py
+++ scripts/halo_profiles.py
@@ -12,12 +12,18 @@ directory = args.directory
 import numpy as np
 import yt
 from yt.analysis_modules.halo_analysis.api import HaloCatalog
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 fields = {
     'gas': ('gas', 'density'),
     'stars': ('deposit', 'stars_density'),
     'dark_matter': ('deposit', 'dark_matter_density')
 }
+
+units = {field: 'Msun/kpc**3' for field in fields.values()}
+units.update({'radius': 'kpc'})
 
 def stars(pfilter, data):
     filter = data[(pfilter.filtered_type, 'particle_type')] == 2
@@ -39,14 +45,18 @@ hc = HaloCatalog(data_ds=ds, halos_ds=halos_ds)
 hc.load()
 halos = hc.halo_list
 
-for index, halo in enumerate(halos):
+def profile_plot(halo, filename):
     com = [halo.quantities['particle_position_x'], halo.quantities['particle_position_y'], halo.quantities['particle_position_z']]
-    sp = ds.sphere(com, (20, 'kpc'))
-    profiles = []
-    labels = []
-    for fieldname, field in fields.iteritems(): 
-        profiles.append(yt.create_profile(sp, 'radius', field))
-        labels.append(fieldname)
-    plot = yt.ProfilePlot.from_profiles(profiles, labels=labels)
-    plot.set_unit('radius', 'kpc')
-    plot.save('%s_%d_profile.png' % (ds, index))
+    sp = ds.sphere(com, (10, 'Mpc'))
+    profiles = yt.create_profile(sp, 'radius', fields.values(), units=units)
+    for fieldname, field in fields.iteritems():
+        plt.loglog(profiles.x, profiles[field], label=fieldname)
+    plt.xlim([profiles.x.min(), profiles.x.max()])
+    plt.xlabel('Radius $[kpc]$')
+    plt.ylabel('$\\rho [M_{\odot} kpc^{-3}]$')
+    plt.legend(loc='best')
+    plt.savefig(filename)
+    plt.clf()
+
+for index, halo in enumerate(halos):
+    profile_plot(halo, '%s_%d_profile.png' % (ds, index))
